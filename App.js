f7d41d37375cd56391a950c89958ee0fd7790a2e
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-08-10 21:51:31 -0700

    Add basic layout of components

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { Constants } from 'expo'
+import Decks from './components/Decks'
+import Cards from './components/Cards'
+import Question from './components/Question'
+import Answer from './components/Answer'
+import Score from './components/Score'
+import NewDeck from './components/NewDeck'
+import NewQuestion from './components/NewQuestion';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    <UdaciStatusBar barStyle="dark-content" />
-      {/* <Decks /> */}
-      {/* <Cards /> */}
-      {/* <Question /> */}
-    <Answer />
-      {/* <Score /> */}
-      {/* <NewDeck /> */}
-      {/* <NewQuestion /> */}
-    </View>
+function UdaciStatusBar({ backgroundColor, ...props }) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar
+                translucent
+                // backgroundColor={backgroundColor}
+                {...props}
+            />
+        </View>
+    )
+}
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <UdaciStatusBar barStyle="dark-content" />
+        {/* <Decks /> */}
+        {/* <Cards /> */}
+        {/* <Question /> */}
+        <Answer />
+        {/* <Score /> */}
+        {/* <NewDeck /> */}
+        {/* <NewQuestion /> */}
+      </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
